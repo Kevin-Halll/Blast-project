744c90cb0138d16e71d19c6a42fb2efc7b308eeb
Dejhon <dejhonsherman6@gmail.com>
2022-03-22 10:15:03 -0500

    Registern api test

--- src/app/Services/blast-service.service.ts
+++ src/app/Services/blast-service.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,12 @@ export class BlastServiceService {
   constructor(private Http: HttpClient) { }
 
   postUser(data:any){
-    return this.Http.post<any>('http://10.44.16.165:8000/api/register', data)
+    // Can be updated in environment folder=>(`${environment.apiAddress}/register`)
+    return this.Http.post<any>(`${environment.apiAddress}/register`, data)
   }
 
   addUser(data:any){
-    return  this.Http.put<any>('http://10.44.16.165:8000/api/register', data)
+    return  this.Http.put<any>(`${environment.apiAddress}/register`, data)
   }
 }
 
--- src/app/components/registration-form/registration-form.component.ts
+++ src/app/components/registration-form/registration-form.component.ts
@@ -1,6 +1,9 @@
+import { BlastServiceService } from './../../Services/blast-service.service';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators} from '@angular/forms'; 
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 import { passwordsMatchValidator } from './confirm-password';
+
 
 @Component({
   selector: 'app-registration-form',
@@ -9,14 +12,75 @@ import { passwordsMatchValidator } from './confirm-password';
 })
 export class RegistrationFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private BlastServiceService: BlastServiceService) { }
 
   submitted: boolean = false;
- 
+  registerForm!: FormGroup;
+  ngOnInit(): void {
+
+    // formgroup information
+  this.registerForm = new FormGroup({
+    firstName : new FormControl('', Validators.required),
+    lastName : new FormControl('', Validators.required),
+    email : new FormControl('', [Validators.required, Validators.pattern(/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/)]),
+    phoneNumber: new FormControl('',[Validators.required, Validators.pattern(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s-]\d{3}[\s-]\d{4}$/)]),
+    address : new FormGroup({
+      streetAddress: new FormControl('',),
+      addressCity: new FormControl('', ),
+      addressParish: new FormControl('' ,)
+    }),
+    dob: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required , Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]),
+    confirmPassword: new FormControl('', Validators.required),
+    },
+    {
+    validators: passwordsMatchValidator
+    }
+  );
+}
+
   // function for after form has been submitted
-  submitForm(registerForm:any){
-    console.log(registerForm.value)
+  addUser(registerForm:FormGroup){
     this.submitted = true;
+  if(registerForm.valid && this.submitted == true){
+
+    const data ={
+      first_name: this.firstName?.value,
+      last_name: this.lastName?.value,
+      email: this.email?.value,
+      phone: this.phoneNumber?.value,
+      password: this.password?.value,
+      password_confirm: this.confirmPassword?.value,
+      address: this.streetAddress?.value,
+      city: this.addressCity?.value,
+      parish: this.addressParish?.value,
+      dob: this.dob?.value,
+    }
+
+    const userInfo = new FormData()
+      userInfo.append('first_name',data.first_name);
+      userInfo.append('last_name',data.last_name);
+      userInfo.append('email',data.email);
+      userInfo.append('phone',data.phone);
+      userInfo.append('password',data.password);
+      userInfo.append('password_confirmation',data.password_confirm);
+      // userInfo.append('address',data.address);
+      // userInfo.append('city',data.city);
+      // userInfo.append('parish',data.parish);
+      userInfo.append('dob',data.dob);
+
+        this.BlastServiceService.postUser(userInfo).subscribe({
+          next:(resp: any)=>{
+            // console.log('User Added');
+            // console.log(resp);
+            alert('Sign up successful');
+            this.registerForm.reset();
+          },
+          error:()=>{
+            console.log('Error while adding user')
+          }
+        })
+      }
   }
 
   // getters for returning error messages
@@ -30,6 +94,10 @@ export class RegistrationFormComponent implements OnInit {
 
   get email(){
     return this.registerForm.get('email')
+  }
+
+  get phoneNumber(){
+    return this.registerForm.get('phoneNumber')
   }
 
   get dob(){
@@ -55,27 +123,5 @@ export class RegistrationFormComponent implements OnInit {
   get addressParish(){
     return this.registerForm.get(['address','addressParish'])
   }
-  registerForm!: FormGroup;
-  ngOnInit(): void {
-
-    // formgroup information
-  this.registerForm = new FormGroup({
-    firstName : new FormControl('', Validators.required),
-    lastName : new FormControl('', Validators.required),
-    email : new FormControl('', [Validators.required, Validators.pattern(/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/)]),
-    address : new FormGroup({
-      streetAddress: new FormControl('', Validators.required),
-      addressCity: new FormControl('', Validators.required),
-      addressParish: new FormControl('' ,Validators.required)
-    }),
-    dob: new FormControl('', Validators.required),
-    password: new FormControl('', [Validators.required , Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]),
-    confirmPassword: new FormControl('', Validators.required),
-  },
-  {
-    validators: passwordsMatchValidator
-  }
-);
-}
 
 }
